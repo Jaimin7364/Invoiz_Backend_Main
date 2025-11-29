e34c9182cc6efbba291039ac6cc08646f099db48
Nirav <nrvmodi@gmail.com>
2025-11-29 11:24:35 +0530

    done

--- models/Business.js
+++ models/Business.js
@@ -74,6 +74,7 @@ const businessSchema = new mongoose.Schema({
   gst_number: {
     type: String,
     trim: true,
+    required: false,
     uppercase: true,
     match: [/^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/, 'Please enter a valid GST number'],
     default: null
